e9361568b6fed59ea92e7dfda027556ff88039a8
Sergey Storchay <r8@r8.com.ua>
2012-06-24 18:42:51 +0300

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+mv ~/dotfiles ~/.dotfiles
+
+git submodule init
+git submodule update
+
+ln -s ~/.dotfiles/vim ~/.vim
+
+rm ~/.bashrc
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+
+rm ~/.zshrc
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+
+rm ~/.vimrc
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+
